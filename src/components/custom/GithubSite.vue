a718409e1e6bf6fd6b2b6ac123268e964aa4dd43
chenpengyin11 <137311072+chenpengyin11@users.noreply.github.com>
2024-06-26 01:15:35 +0800

    Update GithubSite.vue

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-neutral-400">
-    <span>Star on</span>
-    <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
+    <span>绝对大理</span>
+    <a href="https://dl0872.com/" target="_blank" class="text-blue-500">
       GitHub
     </a>
   </div>
